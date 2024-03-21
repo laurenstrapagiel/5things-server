7f3276138807c41c9abad31e1d7b93051ed77a82
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-21 16:20:17 -0600

    joins tables

--- controllers/exercises-controller.js
+++ controllers/exercises-controller.js
@@ -2,30 +2,32 @@ const knex = require("knex")(require("../knexfile"));
 
 const getExercises = async (req, res) => {
   try {
-    let exerciseData = await knex("exercises").select(
-     "id",
-      "user_id",
-      "created_at",
-      "rating_before",
-      "location",
-      "see_1",
-      "see_2",
-      "see_3",
-      "see_4",
-     "see_5",
-      "touch_1",
-      "touch_2",
-      "touch_3",
-      "touch_4",
-      "hear_1",
-      "hear_2",
-      "hear_3",
-      "smell_1",
-      "smell_2",
-      "taste_1",
-      "rating_after"
-    );
+    let exerciseData = await knex("exercises")
+      .select(
+        "exercises.id",
+        "users.id as user_id",
+        "exercises.created_at",
+        "exercises.rating_before",
+        "exercises.location",
+        "exercises.see_1",
+        "exercises.see_2",
+        "exercises.see_3",
+        "exercises.see_4",
+        "exercises.see_5",
+        "exercises.touch_1",
+        "exercises.touch_2",
+        "exercises.touch_3",
+        "exercises.touch_4",
+        "exercises.hear_1",
+        "exercises.hear_2",
+        "exercises.hear_3",
+        "exercises.smell_1",
+        "exercises.smell_2",
+        "exercises.taste_1",
+        "exercises.rating_after"
+      )
 
+      .join("users", "exercises.user_id", "users.id");
     res.status(200).json(exerciseData);
   } catch (error) {
     res.status(500).json({
